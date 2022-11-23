a4fbfb2a436c3c6700d65b5f39023f116a17a154
qy-we <1925407813@qq.com>
2022-11-23 18:37:49 +0800

    提交前端

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,7 +7,8 @@ const PAGE_SIZE = 2000
 export async function getSiteConfig() {
   const res = await request<never, any[]>('/seller/api/homepages', {
     params: {
-      'clientId.equals': CLIENT_ID
+      // '': CLIENT_ID
+      clientId: CLIENT_ID
     }
   })
   return res[0]
@@ -15,7 +16,7 @@ export async function getSiteConfig() {
 
 export async function getCourses(popular?: boolean) {
   const url = popular
-    ? `/seller/api/coursesget/getAllCoursesByConditionsWithTotal?page=0&size=18&clientId=${CLIENT_ID}&tag=hot&isDelete=1&sort=courseIndex,asc`
+    ? `/seller/api/coursesget/getAllHotCoursesByConditionsWithTotal?page=0&size=18&clientId=${CLIENT_ID}&tag=hot&isDelete=1&sort=courseIndex,asc`
     : `/seller/api/coursesget/getAllCoursesByConditionsWithTotal?page=0&size=100&isDelete=1&clientId=${CLIENT_ID}&sort=courseIndex,asc`
 
   return await request<never, { courseList: any[] }>(url)
@@ -38,8 +39,10 @@ export async function getMyCourses(phone: string, page = 0) {
 export async function getCourse(id: string) {
   const res = await request<never, any[]>('/seller/api/courses', {
     params: {
-      'clientId.equals': CLIENT_ID,
-      'courseId.equals': id
+      // 'clientId.equals': CLIENT_ID,
+      // 'courseId.equals': id
+      clientId: CLIENT_ID,
+      courseId: id
     }
   })
   return res[0]
@@ -54,8 +57,10 @@ export async function getTeachars() {
 export async function getStudentOfCourse(courseId: string) {
   const res = await request<never, any[]>('/seller/api/students', {
     params: {
-      'clientId.equals': CLIENT_ID,
-      'courseId.equals': courseId,
+      // 'clientId.equals': CLIENT_ID,
+      // 'courseId.equals': courseId,
+      clientId: CLIENT_ID,
+      courseId: courseId,
       size: PAGE_SIZE
     }
   })
@@ -65,8 +70,10 @@ export async function getStudentOfCourse(courseId: string) {
 export async function getReplayOfCourse(courseId: string) {
   const res = await request<never, any[]>('/seller/api/course-classes', {
     params: {
-      'clientId.equals': CLIENT_ID,
-      'courseId.equals': courseId,
+      // 'clientId.equals': CLIENT_ID,
+      // 'courseId.equals': courseId,
+      clientId: CLIENT_ID,
+      courseId: courseId,
       size: PAGE_SIZE,
       sort: 'startAt,desc'
     }
